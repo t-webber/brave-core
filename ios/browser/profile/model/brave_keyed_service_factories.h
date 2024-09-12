606176682f2bdbd3cc5666c38c77250ff05b0de7
Emerick Rogul <erogul@brave.com>
2024-09-12 14:11:35 -0400

    fixup! `//ios/chrome/browser/browser_state/model` deleted

--- ios/browser/profile/model/brave_keyed_service_factories.h
+++ ios/browser/profile/model/brave_keyed_service_factories.h
@@ -1,10 +1,10 @@
 /* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_IOS_BROWSER_PROFILE_MODEL_KEYED_SERVICE_FACTORIES_H_
-#define BRAVE_IOS_BROWSER_PROFILE_MODEL_KEYED_SERVICE_FACTORIES_H_
+#ifndef BRAVE_IOS_BROWSER_PROFILE_MODEL_BRAVE_KEYED_SERVICE_FACTORIES_H_
+#define BRAVE_IOS_BROWSER_PROFILE_MODEL_BRAVE_KEYED_SERVICE_FACTORIES_H_
 
 namespace brave {
 
@@ -12,4 +12,4 @@ void EnsureProfileKeyedServiceFactoriesBuilt();
 
 }  // namespace brave
 
-#endif  // BRAVE_IOS_BROWSER_PROFILE_MODEL_KEYED_SERVICE_FACTORIES_H_
+#endif  // BRAVE_IOS_BROWSER_PROFILE_MODEL_BRAVE_KEYED_SERVICE_FACTORIES_H_
