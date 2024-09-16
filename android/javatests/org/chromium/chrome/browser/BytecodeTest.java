0dee8476e4a1ecf01f6f42b6dff05c8e85fe23bc
AlexeyBarabash <alexey@brave.com>
2024-09-16 14:27:42 +0300

    [Android] LocationBarMediator.mShouldClearOmniboxOnFocus was removed
    "RetainOmniboxOnFocus" feature is used instead
    
    Related Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/46147680fe334aeec900ec6d76c8b9e443d07452
    
    	Adds flag-guarded support for retaining omnibox on focus.
    
    	This CL adds flag-guarded support for retaining omnibox contents on
    	focus (as opposed to clearing it). When enabled, omnibox contents will
    	also be fully selected (to allow for quick replace) and zero-prefixed
    	suggestions are still shown (to allow for quick search). When enabled,
    	only devices with large screens, attached keyboards, and attached
    	precision pointers are affected.
    
    	Design doc: http://shortn/_g7JfqshAX0
    
    	Before:
    	* google.com: http://shortn/_QQDvhI44dH
    	* NTP w/o paste: http://shortn/_RowwKkjjGM
    	* NTP w/ paste: http://shortn/_9Im63Fv4Ep
    
    	After:
    	* google.com: http://shortn/_29TqnQpVQE
    	* NTP w/o paste: http://shortn/_whJqKzcDnv
    	* NTP w/ paste: http://shortn/_ZnBOkVUvrU
    
    	Bug: 347632178
    	Change-Id: I8c8ceffa766ff7c960f122241ae3fb2efe3654c6
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804846

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -2015,10 +2015,6 @@ public class BytecodeTest {
                         "org/chromium/chrome/browser/omnibox/LocationBarMediator",
                         "mIsLocationBarFocusedFromNtpScroll"));
         Assert.assertTrue(
-                fieldExists(
-                        "org/chromium/chrome/browser/omnibox/LocationBarMediator",
-                        "mShouldClearOmniboxOnFocus"));
-        Assert.assertTrue(
                 fieldExists("org/chromium/chrome/browser/omnibox/LocationBarMediator", "mContext"));
         Assert.assertTrue(
                 fieldExists(
