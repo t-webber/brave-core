155ad2c95f6b82d8521c6135c7cef50217d177f7
AlexeyBarabash <alexey@brave.com>
2024-10-29 00:17:01 +0200

    [Android] New ToolbarProgressBar arg at ToolbarLayout.initialize, TopToolbarCoordinator.ctor
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/cf11983d69ee97087378676eb8fb6273e3b33923
    
    	[Anchor] Refactor ToolbarProgressBar
    
    	This change moves ToolbarProgressBar to live as a sibling of, rather
    	than a child of, the ControlContainer, so that it can be repositioned
    	freely within the app's root layout. This enables a cascading series of
    	cleanups:
    
    	* Progress bar init is now via a stub and happens sooner
    	* The animating view is inflated alongside the progress bar
    	* Anchoring is done with CoordinatorLayout's layoutanchor properties
    	  instead of manually
    	* Various obsolete code removed
    
    	Bug: 369133876
    	Change-Id: I2f7ba17176bd18f14af3b107d27a49d3dfdd3a06
    	Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941322

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -101,6 +101,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
 import org.chromium.chrome.browser.theme.ThemeUtils;
 import org.chromium.chrome.browser.toolbar.ToolbarDataProvider;
+import org.chromium.chrome.browser.toolbar.ToolbarProgressBar;
 import org.chromium.chrome.browser.toolbar.ToolbarTabController;
 import org.chromium.chrome.browser.toolbar.bottom.BottomToolbarConfiguration;
 import org.chromium.chrome.browser.toolbar.bottom.BottomToolbarVariationManager;
@@ -1596,7 +1597,8 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
             BooleanSupplier partnerHomepageEnabledSupplier,
             OfflineDownloader offlineDownloader,
             UserEducationHelper userEducationHelper,
-            ObservableSupplier<Tracker> trackerSupplier) {
+            ObservableSupplier<Tracker> trackerSupplier,
+            ToolbarProgressBar progressBar) {
         super.initialize(
                 toolbarDataProvider,
                 tabController,
@@ -1606,7 +1608,8 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                 partnerHomepageEnabledSupplier,
                 offlineDownloader,
                 userEducationHelper,
-                trackerSupplier);
+                trackerSupplier,
+                progressBar);
 
         BraveMenuButtonCoordinator.setMenuFromBottom(isMenuButtonOnBottom());
     }
