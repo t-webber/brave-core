3e481d5e985193ed6018b96c3178726accc210f7
Artem Samoilenko <artem@brave.com>
2024-12-05 15:54:45 -0500

    [Android] Java fixes for presubmit issues

--- android/java/org/chromium/chrome/browser/rewards/BraveRewardsPanel.java
+++ android/java/org/chromium/chrome/browser/rewards/BraveRewardsPanel.java
@@ -1593,7 +1593,9 @@ public class BraveRewardsPanel
                         });
         int learnMoreIndex = text.indexOf(mActivity.getResources().getString(R.string.learn_more));
 
-        ss.setSpan(clickableSpan, learnMoreIndex,
+        ss.setSpan(
+                clickableSpan,
+                learnMoreIndex,
                 learnMoreIndex + mActivity.getResources().getString(R.string.learn_more).length(),
                 Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return ss;
@@ -1614,7 +1616,10 @@ public class BraveRewardsPanel
                         });
         int clickableTextIndex = mainText.indexOf(clickableText);
 
-        ss.setSpan(clickableSpan, clickableTextIndex, clickableTextIndex + clickableText.length(),
+        ss.setSpan(
+                clickableSpan,
+                clickableTextIndex,
+                clickableTextIndex + clickableText.length(),
                 Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return ss;
     }
