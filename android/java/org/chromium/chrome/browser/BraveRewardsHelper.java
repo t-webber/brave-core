a13c608094775b071efe0042c5ddf8e9266fbf85
Artem Samoilenko <artem@brave.com>
2025-04-15 09:43:06 -0400

    [cr137] [Android] Refactor classes for improved TextViewWithClickableSpans
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/48018d01bf6f0ac186684a9fa73b6a98704ad9c9
    
    Refactor classes for improved TextViewWithClickableSpans
    
    The changes in this CL are preparation for updates in
    https://chromium-review.googlesource.com/c/chromium/src/+/6383822.
    
    Bug: 405441323

--- android/java/org/chromium/chrome/browser/BraveRewardsHelper.java
+++ android/java/org/chromium/chrome/browser/BraveRewardsHelper.java
@@ -574,16 +574,14 @@ public class BraveRewardsHelper implements LargeIconBridge.LargeIconCallback {
 
         ChromeClickableSpan termsOfServiceClickableSpan =
                 new ChromeClickableSpan(
-                        context,
-                        colorRes,
+                        context.getColor(colorRes),
                         (textView) -> {
                             CustomTabActivity.showInfoPage(context, BraveActivity.BRAVE_TERMS_PAGE);
                         });
 
         ChromeClickableSpan privacyPolicyClickableSpan =
                 new ChromeClickableSpan(
-                        context,
-                        colorRes,
+                        context.getColor(colorRes),
                         (textView) -> {
                             CustomTabActivity.showInfoPage(
                                     context, BraveActivity.BRAVE_PRIVACY_POLICY);
@@ -615,8 +613,7 @@ public class BraveRewardsHelper implements LargeIconBridge.LargeIconCallback {
 
         ChromeClickableSpan substringClickableSpan =
                 new ChromeClickableSpan(
-                        context,
-                        colorRes,
+                        context.getColor(colorRes),
                         (textView) -> {
                             onSubstringClicked.onResult(context);
                         });
